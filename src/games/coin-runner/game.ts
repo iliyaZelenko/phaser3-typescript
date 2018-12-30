d1cd2cd0c4da9a3539f18533e428bebc0a119738
digitsensitive <digit.sensitivee@gmail.com>
2018-12-30 20:06:38 +0100

    commit before branch change

--- src/games/coin-runner/game.ts
+++ src/games/coin-runner/game.ts
@@ -23,7 +23,7 @@ const config: GameConfig = {
     keyboard: true
   },
   backgroundColor: "#3A99D9",
-  pixelArt: false
+  render: { pixelArt: true, autoResize: false }
 };
 
 export class Game extends Phaser.Game {
